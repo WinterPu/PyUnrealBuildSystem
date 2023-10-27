71def5ef3a84cc22bd66511f4de5384c68a37cb1
WinterPu <striveant@outllook.com>
2023-10-27 17:08:22 +0800

    Update DeleteDir Methods
    File Names in python could not contains '$'
    however, there are some Android files with names containing dollar signs, such as:
              [GameActivity$VirtualKeyboardInput$VirtualKeyboardInputConnection.class]

--- FileIO/FileUtility.py
+++ FileIO/FileUtility.py
@@ -1,15 +1,23 @@
 import shutil
 import os
 from Utility.HeaderBase import *
-
+import platform
 
 class FileUtility:
     def DeleteFile(path):
         PrintLog("DeleteFile", path)
 
     def DeleteDir(path):
-        if os.path.exists(path):
-            PrintLog("DeleteDir")
-            shutil.rmtree(path)
+        osplatform = platform.platform().lower()
+        if "windows" in osplatform:
+            ## In [Intermediate] Folder, there are some Android files with names containing dollar signs, such as:
+            # [GameActivity$VirtualKeyboardInput$VirtualKeyboardInputConnection.class]
+            command = (
+                r"rmdir /s /q" + '"' + path + '"'
+            )
         else:
-            PrintLog("%s not exists" % path)
+            if os.path.exists(path):
+                PrintLog("DeleteDir")
+                shutil.rmtree(path)
+            else:
+                PrintLog("%s not exists" % path)
