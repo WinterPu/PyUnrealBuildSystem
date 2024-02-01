b88bb88f3b1878ff212c21dfbf2ed3638b1735c9
WinterPu <striveant@outllook.com>
2024-02-01 18:25:29 +0800

    [CMD] add arguements to MacRATrust shell script

--- Command/MacRATrustCommand.py
+++ Command/MacRATrustCommand.py
@@ -4,8 +4,15 @@ import os
 
 class MacRATrustCommand:
     
-    def DoMacTrust(self,project_path):
+    def DoMacTrust(self,project_path,dst_path = "",password = ""):
+        password_command = ""
+        if password_command != "":
+            password_command = password
+            
+        extra_cmd_dst_path = ""
+        if dst_path != "":
+            extra_cmd_dst_path = " -D " + '"'+str(dst_path) + ' "'
         command = (
-            r" sudo bash ./MacRATrust.sh " + '"'+str(project_path) + '"'
+            r" sudo bash ./MacRATrust.sh -P " + '"'+str(project_path) + '"' + extra_cmd_dst_path + " " + password_command
         )
         RUNCMD(command)
--- MacRATrust.sh
+++ MacRATrust.sh
@@ -1,20 +1,45 @@
 #!/bin/bash
 
-## Resolving Trusted Execution Problems
-### 1. 'XXXX.framework' can't be opened because Apple cannot check it for malicious software.
-### 2. 'AppXXX is damaged and can’t be opened. You should move it to the Trash'
+ProjectPath="/Users/admin/Documents/Agora-Unreal-SDK-CPP-Example"
+MacFrameworkPath="Plugins/AgoraPlugin/Source/ThirdParty/AgoraPluginLibrary/Mac/Release"
+DstPath="${ProjectPath}/${MacFrameworkPath}"
 
-echo $1
+helpFunction()
+{
+   echo ""
+   echo "Usage: $0 -P parameterA -D parameterB"
+   echo -P "\t-P Project Path"
+   echo -D "\t-D DstPath: [ProjectPath/MacFrameworkPath], we will finally use this path"
+   echo ""
+   echo "=== Resolving Trusted Execution Problems === "
+   echo "1. 'XXXX.framework' can't be opened because Apple cannot check it for malicious software."
+   echo "2. 'AppXXX is damaged and can’t be opened. You should move it to the Trash'"
+   exit 1 # Exit script after printing help
+}
 
-PathToYourProject=$1
+while getopts ":P:D:" opt
+do
+   case "$opt" in
+      P ) ArgProjectPath="$OPTARG" ;;
+      D ) ArgDstPath="$OPTARG" ;;
+      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
+   esac
+done
 
-if [ -z "" ]
+
+if [ -n "$ArgProjectPath" ] && [ -z "$ArgDstPath" ]
 then
-    PathToYourProject="/Users/admin/Documents/LLS/AgoraExample/"
+   DstPath="${ArgProjectPath}/${MacFrameworkPath}"
+elif [ -n "$ArgDstPath" ]
+then
+   echo "===== Directly Set DstPath ====="
+   DstPath="${ArgDstPath}"
 fi
 
-echo $PathToYourProject
-for file in $(ls "${PathToYourProject}/Plugins/AgoraPlugin/Source/ThirdParty/AgoraPluginLibrary/Mac/Release");
+echo "[ProjectPath] = $ProjectPath"
+echo "[DstPath] = $DstPath "
+
+for file in $(ls "${DstPath}");
 do
-    sudo xattr -r -d com.apple.quarantine "${PathToYourProject}/Plugins/AgoraPlugin/Source/ThirdParty/AgoraPluginLibrary/Mac/Release/${file}"
+    sudo xattr -r -d com.apple.quarantine "${DstPath}/${file}"
 done
