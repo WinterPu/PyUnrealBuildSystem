ffb91b253fd4145e8c85ab5003e8a14ba3c1de0e
WinterPu <striveant@outllook.com>
2024-05-22 14:40:04 +0800

    Update FastLaneCommand.py
    change mobileprovision path to absolute path

--- Command/FastLaneCommand.py
+++ Command/FastLaneCommand.py
@@ -2,13 +2,17 @@ from Command.CommandBase import *
 
 ## brew install ruby
 ## sudo gem install sigh
+from pathlib import Path
 
 class FastLaneCommand:
     None
 
     def IPAResign(self,path_ipa,signing_identity,provisioning_profile):
+        cwd = Path.cwd()
+        path_abs_mobileprovision = cwd.joinpath(provisioning_profile).resolve()
+
         command = (
-            r'fastlane sigh resign  "' + str(path_ipa) +'" --signing_identity '+ '"' + signing_identity + '" '+ '-p ' + '"' +provisioning_profile + '"'
+            r'fastlane sigh resign  "' + str(path_ipa) +'" --signing_identity '+ '"' + signing_identity + '" '+ '-p ' + '"' + str(path_abs_mobileprovision) + '"'
         )
 
         RUNCMD(command)
