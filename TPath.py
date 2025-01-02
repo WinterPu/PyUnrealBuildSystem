00ed08099dc194baf17bf2b22fe9c16b3bc2674e
WinterPu <striveant@outlook.com>
2025-01-02 15:31:51 +0800

    Update TPath.py

--- TPath.py
+++ TPath.py
@@ -5,6 +5,7 @@ ArgParser = argparse.ArgumentParser(description="Windows Path")
 ArgParser.add_argument("-v", default="")
 ArgParser.add_argument("-p", action="store_true")
 ArgParser.add_argument("-r", action="store_true")
+ArgParser.add_argument("-d", action="store_true")
 Args = ArgParser.parse_args()
 val = Args.v
 if val != "":
@@ -12,6 +13,9 @@ if val != "":
     if Args.p:
         path = path.parent
     path_str = str(path)
-    if Args.r:
+    if Args.r: ## forward slash
+        path_str = path_str.replace('\\', '/')
+    if Args.d: ## double backslash
         path_str = path_str.replace('\\', '\\\\')
     print(path_str)
+
